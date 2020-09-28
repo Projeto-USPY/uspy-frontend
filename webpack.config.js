b84a57db0731fdbf9188f54e62e411663e48219d
Lucas Turci <lucas.turci@gmail.com>
2020-09-27 23:41:27 -0300

    Configure typescript. Reconfigure eslint using yarn eslint --init

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,29 @@
 const path = require('path')
 
 module.exports = {
-	entry: path.join(__dirname, 'src', 'index'),
-	output: {
-		filename: 'bundle.js',
-		path: path.join(__dirname, 'dist'),
-		publicPath: '/static/',
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			include: /src/,
-			use: ['babel-loader', 'eslint-loader'],
-		}]
-	},
-	watchOptions: {
-		poll: 1000, // polls every second
-	},
-	devServer: {
-		historyApiFallback: true,
-	}
+  entry: path.join(__dirname, 'src', 'index'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/static/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: ['babel-loader', 'eslint-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  watchOptions: {
+    poll: 1000 // polls every second
+  },
+  devServer: {
+    historyApiFallback: true
+  }
 }
