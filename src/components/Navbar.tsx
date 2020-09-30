0dcd467695ff4159422921730d1a45978bf89cf5
Lucas Turci <lucas.turci@gmail.com>
2020-09-30 18:35:52 -0300

    Create basic navbar using material-ui component library

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import Slide from '@material-ui/core/Slide'
+
+const HideOnScroll: React.FC<any> = ({
+	children
+}) => {
+	const trigger = useScrollTrigger()
+
+	return <Slide appear={false} direction="down" in={!trigger}>
+		{children}
+	</Slide>
+}
+
+interface NavbarProps {
+	buttons: string[]
+}
+
+const Navbar: React.FC<NavbarProps> = (props) => {
+	const { buttons } = props
+	alert(buttons)
+	return <HideOnScroll {...props}>
+		<AppBar>
+			<Toolbar>
+				<Typography variant="h6">Scroll to Hide App Bar</Typography>
+				{buttons.map((title, idx) => <Button color="inherit" key={idx}>{title}</Button>)}
+			</Toolbar>
+
+		</AppBar>
+	</HideOnScroll>
+}
+
+export default Navbar
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Navbar from './components/Navbar'
 
 const App = () => {
-	return <h1> Hello World </h1>
+	return <>
+		<Navbar buttons={['Login', 'Cadastrar']}/>
+		<div style={{ height: '50px' }}></div>
+		<h1> Hello World </h1>
+	</>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
