201ea1cdcb886ae8f1357dd087754695a03b7b67
Lucas Turci <lucas.turci@gmail.com>
2020-09-30 19:59:58 -0300

    Create footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useTheme } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import './style.css'
+interface FooterProps {
+	text: string
+}
+
+const Navbar: React.FC<FooterProps> = ({
+	text
+}) => {
+	const theme = useTheme()
+	return <div className='footer' style={{
+		backgroundColor: theme.palette.primary.main
+	}}>
+		<Typography variant='body2'> {text} </Typography>
+	</div>
+}
+
+export default Navbar
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,22 @@ import ReactDOM from 'react-dom'
 import Navbar from './components/Navbar'
 import Container from '@material-ui/core/Container'
 import theme from './theme'
+import Footer from './components/Footer'
+import './style.css'
 
 const App = () => {
 	return <>
 		<ThemeProvider theme={theme}>
-			<Navbar buttons={['Login', 'Cadastrar']}/>
-			<Container style={{ backgroundColor: 'secondary' }}>
-				<div style={{ height: '50px' }}></div>
-				<h1> Hello World </h1>
-			</Container>
+			<div className='main'>
+				<main>
+					<Navbar buttons={['Login', 'Cadastrar']}/>
+					<Container style={{ backgroundColor: 'secondary' }}>
+						<div style={{ height: '50px' }}></div>
+						<h1> Hello World </h1>
+					</Container>
+				</main>
+				<Footer text='Made with love by Preischadt and Turci'/>
+			</div>
 		</ThemeProvider>
 	</>
 }
