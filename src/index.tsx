7e91e9222bbc33c06d7231f8d9195295f1892f82
Lucas Turci <lucas.turci@gmail.com>
2020-10-12 21:18:25 -0300

    Add Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import { ThemeProvider } from '@material-ui/core'
+import { Switch, Route } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import theme from 'theme'
+import LoginPage from 'pages/LoginPage'
 import HomePage from 'pages/HomePage'
 const App = () => {
 	return <>
 		<ThemeProvider theme={theme}>
-			<HomePage/>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path='/Login' component={LoginPage}/>
+					<Route path='/' component={HomePage}/>
+				</Switch>
+			</BrowserRouter>
 		</ThemeProvider>
 	</>
 }
