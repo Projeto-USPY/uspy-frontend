e6ce3b9dd848e5d983a8e1ceedd080e9ccb3b9e8
Lucas Turci <lucas.turci@gmail.com>
2020-10-12 21:16:13 -0300

    Change secondary color and add variants for light and dark

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,15 +1,20 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 
+// To find out light and dark variations of colors: https://material-ui.com/pt/customization/color/#picking-colors
 const primaryColor = '#68417f'
-const secondaryColor = '#b2d3e1'
+const secondaryColor = '#415c77'
 const theme = createMuiTheme({
 	palette: {
 		primary: {
+			light: '#866798',
 			main: primaryColor,
+			dark: '#482d58',
 			contrastText: '#fff'
 		},
 		secondary: {
+			light: '#677c92',
 			main: secondaryColor,
+			dark: '#2d4053',
 			contrastText: '#000'
 		}
 	}
