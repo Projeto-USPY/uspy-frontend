7f3c90c85e63a4c736e0f43fc9963cd77b37aa76
Lucas Turci <lucas.turci@gmail.com>
2020-12-22 12:42:56 -0300

    Setup configuration for environment variables and dev and production modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,27 @@
 const path = require('path')
+const dotenv = require('dotenv')
+const webpack = require('webpack')
 
-module.exports = {
+// Take care of environment variables configuration
+const env = dotenv.config({
+	path: process.env.MODE === 'dev' ? path.join(__dirname, '.env.dev') : path.join(__dirname, '.env.prod') // choose right path depending of mode of execution
+}).parsed
+const envKeys = Object.keys(env).reduce((prev, next) => {
+	prev[`process.env.${next}`] = JSON.stringify(env[next])
+	return prev
+}, {})
+
+// Options for development mode
+const devOptions = {
+	watchOptions: {
+		poll: 1000 // polls every second
+	},
+	devServer: {
+		historyApiFallback: true
+	}
+}
+
+module.exports = Object.assign({
 	entry: path.join(__dirname, 'src', 'index'),
 	output: {
 		filename: 'bundle.js',
@@ -29,10 +50,9 @@ module.exports = {
 		extensions: ['.tsx', '.ts', '.js'],
 		modules: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'src')]
 	},
-	watchOptions: {
-		poll: 1000 // polls every second
-	},
-	devServer: {
-		historyApiFallback: true
-	}
-}
+	plugins: [
+		new webpack.DefinePlugin(envKeys)
+	]
+},
+(process.env.MODE === 'dev' ? devOptions : {})
+)
