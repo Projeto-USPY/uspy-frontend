fca62ac4bf5a13bf0be279d11406d13926fb76bc
Lucas Turci <lucas.turci@gmail.com>
2020-12-22 12:18:21 -0300

    Implement request to API to provide search data to search engine

--- src/HOCs/withSearchData.tsx
+++ src/HOCs/withSearchData.tsx
@@ -1,24 +1,40 @@
 import React, { createContext, ReactElement, useEffect, useState } from 'react'
+import axios from 'axios'
+export const SearchDataContext: React.Context<any> = createContext(['SCC210', 'Kalinka'])
 
-export const SearchDataContext: React.Context<any> = createContext([])
+const API = axios.create({
+	baseURL: 'http://127.0.0.1:8081',
+	responseType: 'json'
+})
 
-let data: any = null
-export const getData = () => {
+function unique (arr: string[]): string[] {
+	return [...new Set(arr)]
+}
+
+let data: string[] = null
+function getData (setter: Function) {
 	if (data == null) {
-		data = ['SCC210', 'SCC2011', 'Kalinka']
+		API.get('/api/subject/all').then((res: any) => {
+			data = res.data.reduce((cur: string[], course: any) => {
+				return [...cur, ...Object.keys(course.subjects).map((val, idx) => val + ' - ' + course.subjects[val])]
+			}, [])
+			data = unique(data)
+			setter(data)
+		})
 	}
-	return data
+	setter(data)
 }
 
 const withSearchData = (child: ReactElement) => {
 	const [searchData, setSearchData] = useState(data)
 	useEffect(() => {
-		data = getData()
-		setSearchData(data)
+		getData(setSearchData)
 	}, [])
 
 	if (searchData == null) {
-		return <></>
+		return <>
+			{ child }
+		</>
 	} else {
 		return <SearchDataContext.Provider value={searchData}>
 			{child}
