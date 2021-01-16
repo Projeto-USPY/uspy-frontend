e008c351cb676a50fac611f58616ab6f9b6c3c6c
Lucas Turci <lucas.turci@gmail.com>
2021-01-16 16:16:56 -0300

    WIP SubjectPage: Create MessagePanel component for messages such as 'you cant view this resource' or such

---
+++ src/components/MessagePanel/index.tsx
@@ -0,0 +1,27 @@
+import React, { memo } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+
+interface props {
+	message: string
+	height: number
+}
+
+const MessagePanel: React.FC<props> = ({ message, height }) => {
+	const style = {
+		height,
+		flexGrow: 1,
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		border: '1px solid #BBBBBB',
+		boxShadow: 'inset 0 0 3px 1px #BBBBBB'
+	}
+	return <Grid container justify='center' alignItems='stretch'>
+		<div style={style}>
+			<Typography variant='body2'> {message} </Typography>
+		</div>
+	</Grid>
+}
+
+export default memo(MessagePanel)
--- src/components/RequirementsGraph/index.tsx
+++ src/components/RequirementsGraph/index.tsx
@@ -4,6 +4,7 @@ import CircularProgress from '@material-ui/core/CircularProgress'
 import Grid from '@material-ui/core/Grid'
 import { ArcherContainer, ArcherElement } from 'react-archer'
 import { getSubjectRelations } from 'API'
+import MessagePanel from 'components/MessagePanel'
 
 interface BoxProps {
 	name: string
@@ -40,6 +41,8 @@ const RequirementsGraph: React.FC<RequirementsGraphProps> = ({ course, code }) =
 	const [predecessors, setPredecessors] = useState<SubjectInfo[]>([])
 	const [successors, setSuccessors] = useState<SubjectInfo[]>([])
 	const [isLoading, setIsLoading] = useState<boolean>(true)
+
+	const noRequirement = !successors.length && !predecessors.length
 	const cardHeight = Math.max(predecessors?.length, successors?.length, 1) * 50 + 30 // calculate cardHeight from size of arrays
 
 	useEffect(() => {
@@ -57,39 +60,41 @@ const RequirementsGraph: React.FC<RequirementsGraphProps> = ({ course, code }) =
 		targetAnchor: 'left',
 		sourceAnchor: 'right'
 	}))
-	return <ArcherContainer strokeColor="black">
-		<Grid container spacing={3} alignItems="stretch" style={{ height: `${cardHeight}px` }} >
-			{isLoading ? <Grid container justify='center'><Grid item><CircularProgress/></Grid></Grid>
-				: <>
-					<Grid container item direction="column" style={{ height: '100%' }} justify="center" alignItems="center" xs={4}>
-						{predecessors.map(({ code: c }) => <Box
-							key={c}
-							name={c}
-							relations={[{
-								targetId: code,
-								targetAnchor: 'left',
-								sourceAnchor: 'right'
-							}]}
-						/>
-						)}
-					</Grid>
-					<Grid container item direction="column" style={{ height: '100%' }} justify="center" alignItems="center" xs={4}>
-						<Box
-							name={code}
-							relations={mainRelations}
-						/>
-					</Grid>
-					<Grid container item direction="column" style={{ height: '100%' }} justify="center" alignItems="center" xs={4}>
-						{successors.map(({ code }) =>
-							<Box
-								key={code}
-								name={code}
-							/>
-						)}
-					</Grid>
-				</>
-			}
+
+	const content = <Grid container spacing={3} alignItems="stretch" style={{ height: `${cardHeight}px` }} >
+		<Grid container item direction="column" style={{ height: '100%' }} justify="center" alignItems="center" xs={4}>
+			{predecessors.map(({ code: c }) => <Box
+				key={c}
+				name={c}
+				relations={[{
+					targetId: code,
+					targetAnchor: 'left',
+					sourceAnchor: 'right'
+				}]}
+			/>
+			)}
 		</Grid>
+		<Grid container item direction="column" style={{ height: '100%' }} justify="center" alignItems="center" xs={4}>
+			<Box
+				name={code}
+				relations={mainRelations}
+			/>
+		</Grid>
+		<Grid container item direction="column" style={{ height: '100%' }} justify="center" alignItems="center" xs={4}>
+			{successors.map(({ code }) =>
+				<Box
+					key={code}
+					name={code}
+				/>
+			)}
+		</Grid>
+	</Grid>
+
+	return <ArcherContainer strokeColor="black">
+		{isLoading ? <Grid container justify='center'><Grid item><CircularProgress/></Grid></Grid>
+			: noRequirement
+				? <MessagePanel height={200} message="Sem requerimentos ou trancamentos"/>
+				: content}
 
 	</ArcherContainer>
 }
