36cc24756b8dc2d533fc18a2cec499c243d92548
Lucas Turci <lucas.turci@gmail.com>
2021-02-02 19:06:22 -0300

    Separate ImageBlock component declaration into separate file

---
+++ src/components/ImageBlock/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import { useTheme } from '@material-ui/styles'
+
+interface ImageBlockProps {
+	imageSource: string
+	title: string
+	size: 'small' | 'medium' | 'large'
+	caption?: string
+	center?: boolean
+}
+
+const ImageBlock: React.FC<ImageBlockProps> = ({ imageSource, title, size, caption, center = false }) => {
+	const fullWidth = {
+		width: '100%'
+	}
+
+	const theme = useTheme()
+	const isMD = useMediaQuery(theme.breakpoints.up('md'))
+
+	// number of cols for each size
+	let colsXS = 10
+	let colsMD = 10
+	let colsLG = 10
+	if (size === 'small') {
+		colsXS = 12
+		colsMD = 8
+		colsLG = 4
+	} else if (size === 'medium') {
+		colsXS = 12
+		colsMD = 10
+		colsLG = 8
+	} else {
+		colsXS = 12
+		colsMD = 10
+		colsLG = 10
+	}
+
+	const margin = {
+		marginLeft: isMD ? '0 1rem' : '0',
+		marginRight: isMD ? '0 1rem' : '0',
+		marginTop: '2rem',
+		marginBottom: '2rem'
+	}
+
+	return <Grid style={margin} container justify={center ? 'center' : 'flex-start'}>
+		<Grid item xs={colsXS} md={colsMD} lg={colsLG}>
+			<div className='figure' style={fullWidth}>
+				<div><img src={imageSource} alt={title} style={fullWidth}/></div>
+				<figcaption> <Typography variant='caption'> {caption} </Typography></figcaption>
+			</div>
+		</Grid>
+	</Grid>
+}
+
+export default ImageBlock
--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -9,64 +9,17 @@ import CardContent from '@material-ui/core/CardContent'
 import CardHeader from '@material-ui/core/CardHeader'
 import Grid from '@material-ui/core/Grid'
 import Link from '@material-ui/core/Link'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
-import { useTheme, makeStyles } from '@material-ui/core/styles'
-import CheckboxesImage from 'checkboxes.png'
-import AuthenticityCodeImage from 'authenticityCode.png'
 import InputBase from '@material-ui/core/InputBase'
 import Box from '@material-ui/core/Box'
 import Divider from '@material-ui/core/Divider'
 import Button from '@material-ui/core/Button'
+import ImageBlock from 'components/ImageBlock'
+import { makeStyles } from '@material-ui/styles'
 
-interface ImageBlockProps {
-	imageSource: string
-	title: string
-	size: 'small' | 'medium' | 'large'
-	caption?: string
-	center?: boolean
-}
-const ImageBlock: React.FC<ImageBlockProps> = ({ imageSource, title, size, caption, center = false }) => {
-	const fullWidth = {
-		width: '100%'
-	}
+// Images
+import CheckboxesImage from 'checkboxes.png'
+import AuthenticityCodeImage from 'authenticityCode.png'
 
-	const theme = useTheme()
-	const isMD = useMediaQuery(theme.breakpoints.up('md'))
-
-	// number of cols for each size
-	let colsXS = 10
-	let colsMD = 10
-	let colsLG = 10
-	if (size === 'small') {
-		colsXS = 12
-		colsMD = 8
-		colsLG = 4
-	} else if (size === 'medium') {
-		colsXS = 12
-		colsMD = 10
-		colsLG = 8
-	} else {
-		colsXS = 12
-		colsMD = 10
-		colsLG = 10
-	}
-
-	const margin = {
-		marginLeft: isMD ? '0 1rem' : '0',
-		marginRight: isMD ? '0 1rem' : '0',
-		marginTop: '2rem',
-		marginBottom: '2rem'
-	}
-
-	return <Grid style={margin} container justify={center ? 'center' : 'flex-start'}>
-		<Grid item xs={colsXS} md={colsMD} lg={colsLG}>
-			<div className='figure' style={fullWidth}>
-				<div><img src={imageSource} alt={title} style={fullWidth}/></div>
-				<figcaption> <Typography variant='caption'> {caption} </Typography></figcaption>
-			</div>
-		</Grid>
-	</Grid>
-}
 const useStyles = makeStyles((theme) => ({
 	input: {
 		fontFamily: 'Courier',
