83432bd7142a8fc880fc6ff0f2d2d7f0ea37c200
Lucas Turci <lucas.turci@gmail.com>
2021-02-14 15:28:44 -0300

    Change webpack.config.js so that env variables can be set
    - Make webpack.config.js export a function that receives an env variable
    - Change .env.dev to .env, because no .env.prod will ever exist. GCP passes env variables in the command

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,6 @@ const path = require('path')
 const dotenv = require('dotenv')
 const webpack = require('webpack')
 
-let env = {
-	API_URL: process.env.NODE_ENV === 'development' ? 'https://dev.uspy.me' : 'https://prod.uspy.me'
-}
-
-if (process.env.LOCAL) {
-	// Take care of environment variables configuration
-	env = dotenv.config({
-		path: process.env.NODE_ENV === 'development' ? path.join(__dirname, '.env.dev') : path.join(__dirname, '.env.prod') // choose right path depending of mode of execution
-	}).parsed
-}
-const envKeys = Object.keys(env).reduce((prev, next) => {
-	prev[`process.env.${next}`] = JSON.stringify(env[next])
-	return prev
-}, {})
-
 // Options for development mode
 const devOptions = {
 	watchOptions: {
@@ -27,38 +12,55 @@ const devOptions = {
 	}
 }
 
-module.exports = Object.assign({
-	entry: path.join(__dirname, 'src', 'index'),
-	output: {
-		filename: 'bundle.js',
-		path: path.join(__dirname, 'build', 'static'),
-		publicPath: '/static/'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(ts|js)x?$/,
-				exclude: /node_modules/,
-				include: /src/,
-				use: ['babel-loader', 'eslint-loader']
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/,
-				use: ['file-loader']
-			}
+function buildConfig (env) {
+	let envVars = {
+		API_URL: env.mode === 'development' ? 'https://dev.uspy.me' : 'https://prod.uspy.me'
+	}
+	if (env.local) {
+		envVars = dotenv.config({
+			path: path.join(__dirname, '.env')
+		}).parsed
+	}
+	const envKeys = Object.keys(envVars).reduce((prev, next) => {
+		prev[`process.env.${next}`] = JSON.stringify(envVars[next])
+		return prev
+	}, {})
+
+	return Object.assign({
+		entry: path.join(__dirname, 'src', 'index'),
+		output: {
+			filename: 'bundle.js',
+			path: path.join(__dirname, 'build', 'static'),
+			publicPath: '/static/'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(ts|js)x?$/,
+					exclude: /node_modules/,
+					include: /src/,
+					use: ['babel-loader', 'eslint-loader']
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				},
+				{
+					test: /\.(png|svg|jpg|jpeg|gif)$/,
+					use: ['file-loader']
+				}
+			]
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+			modules: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'src')]
+		},
+		plugins: [
+			new webpack.DefinePlugin(envKeys)
 		]
 	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
-		modules: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'src')]
-	},
-	plugins: [
-		new webpack.DefinePlugin(envKeys)
-	]
-},
-(process.env.MODE === 'dev' ? devOptions : {})
-)
+	(env.local ? devOptions : {})
+	)
+}
+
+module.exports = buildConfig
