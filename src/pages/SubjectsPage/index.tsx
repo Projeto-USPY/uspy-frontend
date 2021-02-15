2c89b9e8ed6cada2aa7224687c81d74117bc5cc4
Lucas Turci <lucas.turci@gmail.com>
2021-02-14 22:40:09 -0300

    Make logic of ordering subjects by code and by name

--- src/pages/SubjectsPage/index.tsx
+++ src/pages/SubjectsPage/index.tsx
@@ -7,13 +7,15 @@ import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
 import Grid from '@material-ui/core/Grid'
 import ListItemText from '@material-ui/core/ListItemText'
-import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
 import MuiAccordion from '@material-ui/core/Accordion'
 import MuiAccordionDetails from '@material-ui/core/AccordionDetails'
 import MuiAccordionSummary from '@material-ui/core/AccordionSummary'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import Typography from '@material-ui/core/Typography'
+import { useTheme } from '@material-ui/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { withStyles } from '@material-ui/core/styles'
 import withSubjectsData, { SubjectsDataContext } from 'HOCs/withSubjectsData'
 import { SubjectInfo } from 'types/Subject'
@@ -74,34 +76,67 @@ function renderRow (c: string, s: SubjectInfo, clickCallback: Function) {
 
 interface SubjectListProps {
 	arr: CourseInfo
+	sortByCode: boolean
 }
-const SubjectList: React.FC<SubjectListProps> = ({ arr }) => {
+const SubjectList: React.FC<SubjectListProps> = ({ arr, sortByCode }) => {
 	const [open, setOpen] = useState<boolean>(false)
 
 	const history = useHistory()
 	const clickItem = (courseCode: string, code: string) => {
 		history.push(`${history.location.pathname}/${courseCode}/${code}`)
 	}
-	const list = useMemo(() => {
-		const code = arr.code
-		return arr.subjects.map(s => renderRow(code, s, clickItem))
-	}, [])
+
+	const getList = (course: CourseInfo, sortByCode: boolean) => {
+		const code = course.code
+		course.subjects.sort((s1: SubjectInfo, s2: SubjectInfo) => {
+			if (sortByCode) {
+				return s1.code < s2.code ? -1 : s1.code === s2.code ? 0 : 1
+			} else {
+				return s1.name < s2.name ? -1 : s1.name === s2.name ? 0 : 1
+			}
+		})
+		return course.subjects.map(s => renderRow(code, s, clickItem))
+	}
+
+	const listByCode = useMemo(() => getList(arr, true), [])
+	const listByName = useMemo(() => getList(arr, false), [])
 
 	return <Accordion square expanded={open} onChange={() => { setOpen(!open) }} TransitionProps={{ timeout: 20 }}>
 		<AccordionSummary expandIcon={<ExpandMoreIcon />}><Typography><strong>{arr.name}</strong></Typography></AccordionSummary>
 		<AccordionDetails>
 			<List style={{ width: '100%', fontFamily: 'Raleway, sans-serif' }} disablePadding>
-				{list}
+				<div hidden={sortByCode}> {listByName} </div> {/* for otimization, make two */}
+				<div hidden={!sortByCode}> {listByCode} </div>
 			</List>
 		</AccordionDetails>
 	</Accordion>
 }
 
 const Accordions = () => {
+	const [sortByCode, setSortByCode] = useState<boolean>(true)
 	const coursesData = useContext(SubjectsDataContext)
 
+	const theme = useTheme()
+	const isDesktop = useMediaQuery(theme.breakpoints.up('sm'))
+
 	if (!coursesData.length) return <Grid container justify='center' alignItems='center'><Grid item><CircularProgress /></Grid></Grid>
-	else return coursesData.map((c: CourseInfo) => <SubjectList key={c.name} arr={c}/>)
+	else {
+		return <Grid container spacing={3} direction='column'>
+			<Grid container item direction='row' justify={isDesktop ? 'flex-end' : 'center'}>
+				<Grid item>
+					<input type='radio' name='order' id='code' onChange={() => setSortByCode(true)} checked={sortByCode}/>
+					<label htmlFor='code'> Ordenar por código </label>
+				</Grid>
+				<Grid item>
+					<input type='radio' name='order' id='name' onChange={() => setSortByCode(false)} checked={!sortByCode}/>
+					<label htmlFor='name'> Ordenar por nome </label>
+				</Grid>
+			</Grid>
+			<Grid item>
+				{coursesData.map((c: CourseInfo) => <SubjectList key={c.name} arr={c} sortByCode={sortByCode}/>)}
+			</Grid>
+		</Grid>
+	}
 }
 
 const SubjectsPage = () => {
@@ -113,9 +148,9 @@ const SubjectsPage = () => {
 			<Container>
 				<Typography> As disciplinas estão organizadas por curso: </Typography>
 				<br></br>
-				<Paper variant='outlined'>
+				<Box>
 					{withSubjectsData(<Accordions/>)}
-				</Paper>
+				</Box>
 			</Container>
 		</main>
 		<Footer text='Made with love by Preischadt and Turci'/>
