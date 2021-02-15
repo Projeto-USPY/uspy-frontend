2a813a7ca973930dffe5f1560efae1412ddc3411
Lucas Turci <lucas.turci@gmail.com>
2021-02-15 16:17:47 -0300

    WIP Profile Page

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import HomePage from 'pages/HomePage'
 import TeachersPage from 'pages/TeachersPage'
 import SubjectsPage from 'pages/SubjectsPage'
 import SubjectPage from 'pages/SubjectPage'
+import SettingsPage from 'pages/SettingsPage'
 import NotFoundPage from 'pages/NotFoundPage'
 import 'global.css'
 
@@ -22,6 +23,7 @@ const App = () => {
 					<Route exact path='/Cadastro' component={RegisterPage}/>
 					<Route exact path='/Professores' component={TeachersPage}/>
 					<Route exact path='/Disciplinas' component={SubjectsPage}/>
+					<Route exact path='/Perfil' component={SettingsPage}/>
 					<Route exact path='/Disciplinas/:course/:code' component={SubjectPage}/>
 					<Route exact path='/' component={HomePage}/>
 					<Route path='/' component={NotFoundPage}/>
---
+++ src/pages/SettingsPage/index.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import Divider from '@material-ui/core/Divider'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Navbar from 'components/Navbar'
+import BreadCrumb from 'components/Breadcrumb'
+import InputPassword from 'components/PasswordInput'
+
+import { useTheme } from '@material-ui/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+
+const textFieldCommonProps = {
+	variant: 'outlined',
+	color: 'secondary',
+	size: 'medium',
+	fullWidth: true,
+	style: {
+		height: '50px'
+	}
+}
+
+// Returns true if pwd has 8+ characters with at least one number and one special character
+function goodPassword (pwd: string) {
+	return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(pwd)
+}
+
+const SettingsPage = () => {
+	const [newPwd, setNewPwd] = useState<string>('')
+	const [showPwdError, setShowPwdError] = useState<boolean>(false)
+
+	const theme = useTheme()
+	const isDesktop = useMediaQuery(theme.breakpoints.up('sm'))
+
+	const pwdOk = goodPassword(newPwd) || !showPwdError
+	return <div className='main'>
+		<main>
+			<Navbar/>
+			<div style={{ height: '94px' }}></div>
+			<Container>
+				<Grid container alignItems='center' style={{ height: '50px' }}>
+					<BreadCrumb links={[{
+						url: '/',
+						text: 'Home'
+					}, {
+						url: '/Perfil',
+						text: 'Perfil'
+					}]}/>
+				</Grid>
+
+				<br/><br/>
+				<Typography variant='h5'> <b>Alterar Senha</b> </Typography>
+				<Divider/>
+				<br/>
+				<Typography> A senha deve conter no mínimo 8 caracteres, com pelo menos um símbolo e um número. </Typography>
+				<br/>
+				<br/>
+				<Grid container direction={isDesktop ? 'row' : 'column'} spacing={2}>
+					<Grid item>
+						<InputPassword
+							label="Senha Antiga"
+							id="old_pwd"
+							{...textFieldCommonProps}
+						/>
+					</Grid>
+					<Grid item>
+						<InputPassword
+							label="Senha Nova"
+							id="new_pwd"
+							value={newPwd}
+							error={!pwdOk}
+							helperText={!pwdOk ? 'Senha não satisfaz os requisitos' : ''}
+							onBlur={() => setShowPwdError(true)}
+							onChange={(evt: any) => setNewPwd(evt.target.value)}
+							{...textFieldCommonProps}
+						/>
+					</Grid>
+
+					<Grid item>
+						<Button
+							color="secondary"
+							size="medium"
+							variant='outlined'
+							style={{ height: '55px' }}
+							fullWidth={!isDesktop}
+						>
+							Confirmar
+						</Button>
+					</Grid>
+				</Grid>
+			</Container>
+		</main>
+	</div>
+}
+
+export default SettingsPage
