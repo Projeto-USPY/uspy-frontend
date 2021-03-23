8a4f3bbd1676bf8055f0e5a9f97a9619519ddc6f
Lucas Turci <lucas.turci@gmail.com>
2021-03-23 20:54:18 -0300

    Centralize Home Page better

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, useHistory } from 'react-router-dom'
 
 import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
 import { useTheme } from '@material-ui/core/styles'
 import Typography from '@material-ui/core/Typography'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
@@ -21,12 +22,14 @@ const HomePage = () => {
 	const clickItem = (courseCode: string, courseSpecialization: string, code: string) => {
 		history.push(`/Disciplinas/${courseCode}/${courseSpecialization}/${code}`)
 	}
-	return <>
-		<div className='main'>
-			<main>
-				<Navbar/>
-				<Container style={{ backgroundColor: 'secondary' }}>
-					<div style={{ height: '150px' }}></div>
+	return <Grid container direction='column' justify='space-between' alignItems='stretch' style={{ height: '100%' }}>
+		<Grid item>
+			<Navbar/>
+			<div style={{ height: '64px' }}></div>
+		</Grid>
+		<Grid item container direction='column' justify='center' alignItems='stretch' xs>
+			<Grid item>
+				<Container>
 					<center><img width={isLarge ? 350 : '75%'} src={logo}/></center>
 					{/* <Typography align='justify' variant='body1'>O {'<placeholder nome>'} é um sistema colaborativo em que você pode consultar estatísticas e reviews sobre disciplinas e professores do ICMC. Para colaborar, você deve submeter um PDF do seu resumo escolar, mas fique tranquilo, todos os dados são anônimos, criptografados e armazenados sigilosamente. Para começar, inscreva-se com seu email USP, ou faça login.</Typography> */}
 					{withSearchData(<GeneralSearch handleChange={clickItem} />)}
@@ -36,10 +39,12 @@ const HomePage = () => {
 						<Link to='/Disciplinas'> <Typography variant='caption' color='secondary'>Ver lista de disciplinas</Typography> </Link>
 					</div>
 				</Container>
-			</main>
+			</Grid>
+		</Grid>
+		<Grid item>
 			<Footer text='Made with love by Preischadt and Turci'/>
-		</div>
-	</>
+		</Grid>
+	</Grid>
 }
 
 export default HomePage
