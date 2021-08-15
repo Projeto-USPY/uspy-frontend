f9994b06023415b7588960d066a74735be5d7a00
Lucas Turci <lucas.turci@gmail.com>
2021-08-15 16:35:01 -0300

    Cypress set up

---
+++ cypress/integration/main.spec.js
@@ -0,0 +1,12 @@
+// main.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Basic testing', () => {
+	it('Should be able to visit home page', () => {
+		cy.visit('/')
+	})
+})
