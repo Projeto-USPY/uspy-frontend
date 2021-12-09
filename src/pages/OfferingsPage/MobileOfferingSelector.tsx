Projeto-USPY/uspy-frontend

commit f6503d4f5b2924ccc390ee9165f4583dd666f536

Fix small css bug

src/global.css
@@ -103,6 +103,7 @@ b {
 	position: sticky;
 	top: 0px;
 	background-color: #A77BC3;
+	z-index: 1;
 }
 
 .offering-review-form-accordion-content {

commit 5a05e996f34e624083b7af9d98d44bbd0f52cd50

Fix bug where small review box expand button show on top of mobile selector
- Bug was introduced in commit f6503d4, where z-index of expand button
was increase. Fix by increasing z-index of mobile selector divs
- Also improve slide out animation when selector closes

src/global.css
@@ -42,8 +42,8 @@ b {
 	top: 0;
 	overflow-y: auto;
 	overflow-x: hidden;
-	background: white;
 	height: calc(100% - 42px);
+	z-index: 1;
 }
 
 .mobile-offering-selector-button {
@@ -52,6 +52,7 @@ b {
 	height: 42px;
 	display: flex;
 	align-items: center;
+	z-index: 1;
 }
 
 .offering-reviews-feed {

src/pages/OfferingsPage/MobileOfferingSelector.tsx
@@ -29,7 +29,7 @@ const MobileOfferingSelector: React.FC<PropsType> = ({ open, close, offerings, s
 	return <div >
 		<div className='full-width mobile-offering-selector-list'>
 			<Slide direction='down' in={open && !isClosing} mountOnEnter unmountOnExit timeout={500}>
-				<div>
+				<div style={{ background: 'white' }}>
 					<OfferingsList list={offerings} selected={selected} setSelected={handleSelect} />
 				</div>
 			</Slide>