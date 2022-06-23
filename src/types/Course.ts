f97103d4c9c5220440102c6fa9ee71b0f528d696
tprei <thiagop@usp.br>
2022-06-24 00:01:46 +0200

    add new types

--- src/types/Course.ts
+++ src/types/Course.ts
@@ -1,9 +1,19 @@
 import { SubjectInfo } from 'types/Subject'
 
+export interface Institute {
+	name: string
+	code: number
+}
+
 export interface Course {
 	name: string
 	code: string
 	specialization: string
+}
+
+export interface CourseComplete extends Course {
+	shift: string
+	subjects: any
 }
 
 export interface CourseWithSubjects extends Course {
