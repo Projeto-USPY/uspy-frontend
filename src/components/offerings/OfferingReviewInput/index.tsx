73e90fe9f7ff79002d6759753440b520b9b76865
Lucas Turci <lucas.turci@gmail.com>
2023-07-04 21:04:08 +0100

    Fix bug #60: Texto sobreposto ao compor review de oferecimento e trocar pra outro antes de publicar

--- src/components/offerings/OfferingReviewInput/index.tsx
+++ src/components/offerings/OfferingReviewInput/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useContext, useMemo } from 'react'
 
 import './style.css'
+import OfferingContext from 'contexts/OfferingContext'
 
 interface PropsType {
 	content: string
@@ -22,6 +23,10 @@ const OfferingReviewInput: React.FC<PropsType> = ({
 	const ref1 = React.useRef(null)
 	const ref2 = React.useRef(null)
 
+	const { professor, course, specialization, code } =
+		useContext(OfferingContext)
+
+
 	const style = {
 		height: `${1.1876 * rows}em`,
 	}
@@ -31,26 +36,31 @@ const OfferingReviewInput: React.FC<PropsType> = ({
 		ref2.current.scrollTop = ref1.current.scrollTop
 	}
 
+	const [reRenderEditor, setReRenderEditor] = React.useState(0)
+	React.useEffect(() => {
+		setReRenderEditor(reRenderEditor + 1)
+	}, [disabled, course, specialization, code, professor])
+
+	const editorDiv = useMemo(() => {
+		return <div
+			ref={ref1}
+			style={style}
+			key={reRenderEditor}
+			contentEditable={!disabled}
+			suppressContentEditableWarning
+			onScroll={update}
+			onInput={update}
+		>
+			{content}
+		</div>
+	}, [reRenderEditor])
+
 	return (
 		<div
 			className="offering-review-input-wrapper"
 			style={disabled ? { color: 'rgba(0, 0, 0, 0.38)' } : {}}
 		>
-			{React.useMemo(
-				() => (
-					<div
-						ref={ref1}
-						style={style}
-						contentEditable={!disabled}
-						suppressContentEditableWarning
-						onScroll={update}
-						onInput={update}
-					>
-						{content}
-					</div>
-				),
-				[disabled, disabled ? content : '', rows],
-			)}
+			{editorDiv}
 			<div ref={ref2} style={style} className="highlight-mirror">
 				{content.substr(0, limit)}
 				<span>{content.substr(limit)}</span>
