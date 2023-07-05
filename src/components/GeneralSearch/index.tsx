9e8602fb059fb343643405a6e5ecaa2314c9ce2f
Lucas Turci <lucas.turci@gmail.com>
2023-07-06 00:54:27 +0100

    [Bug Fix][Home Page Search] Reset subjects when changing institute

--- src/components/GeneralSearch/index.tsx
+++ src/components/GeneralSearch/index.tsx
@@ -10,7 +10,7 @@ import DomainIcon from '@material-ui/icons/Domain'
 import LayersIcon from '@material-ui/icons/Layers'
 import Autocomplete from '@material-ui/lab/Autocomplete'
 
-import { CourseComplete, Institute, Course } from 'types/Course'
+import { Institute, Course, CourseComplete } from 'types/Course'
 
 import api from 'API'
 import { matchSorter } from 'match-sorter'
@@ -19,6 +19,58 @@ import { getInitials } from 'utils'
 import SearchSelector from './SearchSelector'
 
 import './style.css'
+import { useMySnackbar } from 'hooks'
+import { SubjectKey } from 'types/Subject'
+
+const getParametersFromStorage = (): [Institute, Course] => {
+	const instituteCode = localStorage.getItem('selected-institute')
+	const courseAndSpec = localStorage.getItem('selected-course')
+	if (!(instituteCode && courseAndSpec)) {
+		return [null, null]
+	}
+
+	const institute = instituteCode && {
+		code: instituteCode,
+		name: '',
+	}
+	const [courseCode, spec] = courseAndSpec.split('-')
+	const course = courseAndSpec && {
+		code: courseCode,
+		specialization: spec,
+		name: '',
+	}
+	return [institute, course]
+}
+
+const [savedInstitute, savedCourse] = getParametersFromStorage()
+
+const getSubjectLabel = (opt: SubjectKey): string => {
+	if (!opt?.code) return ''
+	return opt.code + ' - ' + opt.name
+}
+
+const getCourseLabel = (opt: Partial<CourseComplete>): string => {
+	if (!opt?.code) return ''
+	return `${opt.name} (${getInitials(opt.name)}) (${opt?.shift ?? ''})`
+}
+
+const getInstituteLabel = (opt: Institute): string => {
+	if (!opt?.code) return ''
+	return `${opt.name} (${getInitials(opt.name)})`
+}
+
+const autocompleteFilterOptions = (
+	options: SubjectKey[],
+	{ inputValue }: any,
+) => {
+	return matchSorter<SubjectKey>(options, inputValue, {
+		keys: [
+			{
+				key: getSubjectLabel,
+			},
+		],
+	}).slice(0, 40)
+}
 
 interface GeneralSearchInputProps {
 	handleChange: (course: string, specialization: string, code: string) => void
@@ -27,32 +79,67 @@ interface GeneralSearchInputProps {
 const GeneralSearch: React.FC<GeneralSearchInputProps> = ({ handleChange }) => {
 	const theme = useTheme()
 	const isLarge = useMediaQuery(theme.breakpoints.up('sm'))
+	const notify = useMySnackbar()
 
-	const [course, setCourse] = useState<[string, string]>(['', ''])
-	const [institute, setInstitute] = useState<string>('55')
+	const [institute, setInstitute] = useState<Institute>(savedInstitute)
+	const [course, setCourse] = useState<Course>(savedCourse)
 
-	// Institutes useEffect - run on mount
-	const [instituteOptions, setInstituteOptions] = useState<Institute[]>()
+	const [instituteOptions, setInstituteOptions] = useState<Institute[]>([])
+	const [courseOptions, setCourseOptions] = useState<Course[]>([])
+
+	const [instituteInputCollapse, setInstituteInputCollapse] =
+		useState<boolean>(savedInstitute === null)
+	const [courseInputCollapse, setCourseInputCollapse] = useState<boolean>(
+		savedCourse === null,
+	)
+
 	useEffect(() => {
-		api.getInstitutes().then(res => {
-			setInstituteOptions(res)
-		})
+		api.getInstitutes()
+			.then(res => {
+				setInstituteOptions(res)
+				if (institute) {
+					const entry = res.find(i => i.code === institute.code)
+					setInstitute(entry)
+				}
+			})
+			.catch(err => {
+				notify('Erro desconhecido ao carregar institutos', 'error')
+				console.error(err)
+			})
 	}, [])
 
-	// Courses useEffect - run on change of institute
-	const [courseOptions, setCourseOptions] = useState<Course[]>([])
 	useEffect(() => {
-		api.getCourses(institute).then(res => {
-			setCourseOptions(res)
-		})
+		if (!institute || !institute.code) {
+			return
+		}
+		api.getCourses(institute.code)
+			.then(res => {
+				setCourseOptions(res)
+				if (course) {
+					const entry = res.find(c => c.code === course.code)
+					setCourse(entry ?? course)
+				}
+			})
+			.catch(err => {
+				if (err.status === 404) {
+					notify('Curso não encontrado', 'error')
+				} else {
+					notify(
+						`Erro desconhecido ao carregar cursos para ${institute.name}`,
+						'error',
+					)
+					console.error(err)
+				}
+			})
 	}, [institute])
 
 	//  Subjects useEffect - run on change of institute and course
 	const [subjectOptions, setSubjectOptions] = useState([])
 	useEffect(() => {
-		const [courseCode, specializationCode] = course
-		api.getSubjectSearch(institute, courseCode, specializationCode).then(
-			(res: CourseComplete) => {
+		if (!course || !institute) return
+		const { code: courseCode, specialization } = course
+		api.getSubjectSearch(institute.code, courseCode, specialization)
+			.then(res => {
 				const data = Object.keys(res.subjects).map(val => ({
 					course: res.code,
 					specialization: res.specialization,
@@ -60,62 +147,22 @@ const GeneralSearch: React.FC<GeneralSearchInputProps> = ({ handleChange }) => {
 					name: res.subjects[val],
 				}))
 				setSubjectOptions(data)
-			},
-		)
+			})
+			.catch(err => {
+				if (err.status === 404) {
+					notify('Nenhuma disciplina encontrada', 'error')
+				} else {
+					notify('Erro desconhecido ao carregar disciplinas', 'error')
+					console.error(err)
+				}
+			})
 	}, [course, institute])
 
-	const [searchDisabled, setSearchDisabled] = useState<boolean>(true)
-
-	const getOptionLabel = (opt: any) => {
-		if (!opt.code) return opt
-		return opt.code + ' - ' + opt.name
-	}
-
-	const autocompleteFilterOptions = (
-		options: string[],
-		{ inputValue }: any,
-	) => {
-		return matchSorter(options, inputValue, {
-			keys: [getOptionLabel],
-		}).slice(0, 40)
-	}
-
-	const onChange = (
-		_: unknown,
-		value: { course: string; specialization: string; code: string },
-	) => {
+	const onChange = (_evt: unknown, value: SubjectKey) => {
 		handleChange(value.course, value.specialization, value.code)
 	}
 
-	let selectedInstitute: string, selectedCourse: string
-	const [domainCollapse, setDomainCollapse] = useState(() => {
-		return false
-	})
-	const [layersCollapse, setLayersCollapse] = useState(() => {
-		return false
-	})
-
-	useEffect(() => {
-		// get chosen options from memory
-		selectedInstitute = localStorage.getItem('selected-institute')
-		selectedCourse = localStorage.getItem('selected-course')
-
-		if (selectedInstitute) {
-			setInstitute(selectedInstitute)
-
-			if (selectedCourse) {
-				const courseTokens = selectedCourse.split('-')
-				if (courseTokens.length === 2) {
-					setCourse([courseTokens[0], courseTokens[1]])
-				}
-
-				setSearchDisabled(false)
-			}
-		} else {
-			setLayersCollapse(!domainCollapse)
-			setDomainCollapse(!layersCollapse)
-		}
-	}, [])
+	const searchDisabled = !institute || !course
 
 	return (
 		<Container>
@@ -135,35 +182,42 @@ const GeneralSearch: React.FC<GeneralSearchInputProps> = ({ handleChange }) => {
 						item>
 						<DomainIcon
 							onClick={() => {
-								setDomainCollapse(!domainCollapse)
+								setInstituteInputCollapse(
+									!instituteInputCollapse,
+								)
 							}}
-							cursor="pointer"></DomainIcon>
+							cursor="pointer"
+						/>
 					</Grid>
 					<Grid
 						item // institute input
-						xs={domainCollapse ?? true}
+						xs={instituteInputCollapse ?? true}
 						style={{
 							justifyContent: 'center',
 							alignItems: 'center',
-							minWidth: !isLarge && domainCollapse ? '100%' : '0',
+							minWidth:
+								!isLarge && instituteInputCollapse
+									? '100%'
+									: '0',
 						}}>
-						<Collapse in={domainCollapse}>
+						<Collapse in={instituteInputCollapse}>
 							<SearchSelector
-								hidden={!domainCollapse}
+								hidden={!instituteInputCollapse}
 								options={instituteOptions}
 								onChange={(val: Institute) => {
-									setInstitute(val.code)
-									localStorage.setItem(
-										'selected-institute',
-										val.code,
-									)
+									setInstitute(val)
+									setCourse(null)
+									setSubjectOptions([])
+									if (val) {
+										localStorage.setItem(
+											'selected-institute',
+											val.code,
+										)
+									}
 								}}
+								value={institute}
 								label="Procure por um instituto"
-								getOptionLabel={option =>
-									`${option.name} (${getInitials(
-										option.name,
-									)})`
-								}
+								getOptionLabel={getInstituteLabel}
 							/>
 						</Collapse>
 					</Grid>
@@ -172,34 +226,35 @@ const GeneralSearch: React.FC<GeneralSearchInputProps> = ({ handleChange }) => {
 						item>
 						<LayersIcon
 							onClick={() => {
-								setLayersCollapse(!layersCollapse)
+								setCourseInputCollapse(!courseInputCollapse)
 							}}
-							cursor="pointer"></LayersIcon>
+							cursor="pointer"
+						/>
 					</Grid>
 					<Grid // course input
 						item
 						xs={true}
 						style={{
-							minWidth: !isLarge && layersCollapse ? '100%' : '0',
+							minWidth:
+								!isLarge && courseInputCollapse ? '100%' : '0',
 						}}>
-						<Collapse in={layersCollapse}>
+						<Collapse in={courseInputCollapse}>
 							<SearchSelector
-								hidden={!layersCollapse}
+								hidden={!courseInputCollapse}
 								options={courseOptions}
 								onChange={(val: Course) => {
-									setCourse([val.code, val.specialization])
-									localStorage.setItem(
-										'selected-course',
-										`${val.code}-${val.specialization}`,
-									)
-									setSearchDisabled(false)
+									setCourse(val)
+									setSubjectOptions([])
+									if (val) {
+										localStorage.setItem(
+											'selected-course',
+											`${val.code}-${val.specialization}`,
+										)
+									}
 								}}
+								value={course}
 								label="Procure por um curso"
-								getOptionLabel={option =>
-									`${option.name} (${getInitials(
-										option.name,
-									)}) (${option.shift ?? ''})`
-								}
+								getOptionLabel={getCourseLabel}
 							/>
 						</Collapse>
 					</Grid>
@@ -214,8 +269,9 @@ const GeneralSearch: React.FC<GeneralSearchInputProps> = ({ handleChange }) => {
 							openOnFocus
 							className="inputfield"
 							options={subjectOptions}
+							disabled={subjectOptions.length === 0}
 							onChange={onChange}
-							getOptionLabel={getOptionLabel}
+							getOptionLabel={getSubjectLabel}
 							filterOptions={autocompleteFilterOptions}
 							ListboxProps={{
 								style: {
